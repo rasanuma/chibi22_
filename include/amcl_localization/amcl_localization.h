0763e74163ef23b48c264b5df56b3f23844434d4
amsl <amslab@meiji.ac.jp>
2022-05-04 15:09:40 +0900

    bag ok

--- include/amcl_localization/amcl_localization.h
+++ include/amcl_localization/amcl_localization.h
@@ -41,9 +41,10 @@ class AMCL
         double init_y_sigma;
         double init_yaw_sigma;
         double range_sigma;
-        double move_dist_sigma; //動作更新(距離)
-        double move_yaw_sigma;  //動作更新(回転)
-        double MAX_RANGE;
+        double dxy_sigma; //動作更新(距離)
+        double dyaw_sigma;  //動作更新(回転)
+        double max_range;
+        double range_reso;
         double ANGLE_STEP;
         double ALPHA_SLOW;
         double ALPHA_FAST;
@@ -53,10 +54,13 @@ class AMCL
         bool flag_map;
         bool flag_first;
 
+        double sum_weight;
         std::vector<Particle> p_array;
         std::vector<Particle> p_move_array;
         sensor_msgs::LaserScan scan;
         nav_msgs::Odometry odom;
+        nav_msgs::Odometry current_odom;
+        nav_msgs::Odometry previous_odom;
         nav_msgs::OccupancyGrid map;
         geometry_msgs::PoseWithCovarianceStamped amcl_pose;
         geometry_msgs::PoseArray pose_array;
@@ -68,10 +72,12 @@ class AMCL
         void create_particles(double, double, double, double, double, double);
         void p_motion_update();
         double calc_weight(AMCL::Particle &);
+        double range_from_map(double, AMCL::Particle);
         double gaussian(double, double, double);
         void resampring_selection();
         void reset();
         void resampring();
+        // double resampring();
         void output_pose();
         ros::NodeHandle nh;
         ros::NodeHandle private_nh;
--- src/amcl_localization.cpp
+++ src/amcl_localization.cpp
@@ -14,9 +14,10 @@ AMCL::AMCL():private_nh("~")
     private_nh.param("init_y_sigma",init_y_sigma,{0.5});
     private_nh.param("init_yaw_sigma",init_yaw_sigma,{0.5});
     private_nh.param("range_sigma",range_sigma,{0.5});
-    private_nh.param("move_dist_sigma",move_dist_sigma,{1});
-    private_nh.param("move_yaw_sigma",move_yaw_sigma,{0.5});
-    private_nh.param("MAX_RANGE",MAX_RANGE,{5});
+    private_nh.param("dxy_sigma",dxy_sigma,{0.3});
+    private_nh.param("dyaw_sigma",dyaw_sigma,{0.3});
+    private_nh.param("max_range",max_range,{5});
+    private_nh.param("range_reso",range_reso,{0.5});
     private_nh.param("ANGLE_STEP",ANGLE_STEP,{1});
     private_nh.param("ALPHA_SLOW",ALPHA_SLOW,{1});
     private_nh.param("ALPHA_FAST",ALPHA_FAST,{5});
@@ -31,7 +32,6 @@ AMCL::AMCL():private_nh("~")
 
     amcl_pose.header.frame_id = "map";
     pose_array.header.frame_id = "map";
-
     flag_scan = false;
     flag_odom = false;
     flag_map  = true;
@@ -54,23 +54,23 @@ void AMCL::map_callback(const nav_msgs::OccupancyGrid::ConstPtr &msg)
 {
     map = *msg;
     flag_map = true;
-    // Particle p;
 }
 
 void AMCL::create_particles(double x, double y, double yaw, double x_sigma, double y_sigma, double yaw_sigma)
 {
-            printf("b2\n");
+            //printf("b2\n");
     std::vector<Particle> p_init;
     for(int i=0;i<N;i++) {
         Particle p(N);
         std::normal_distribution<> dist_x(x, x_sigma);
-        p.p_pose.pose.pose.position.x=dist_x(engine);//****************
         std::normal_distribution<> dist_y(y, y_sigma);
-        p.p_pose.pose.pose.position.y=dist_y(engine);
-        // yaw = tf::getYaw(p.pose.pose.pose.orientation);//*******************
         std::normal_distribution<> dist_yaw(yaw, yaw_sigma);
+        p.p_pose.pose.pose.position.x=dist_x(engine);
+        p.p_pose.pose.pose.position.y=dist_y(engine);
         p.p_yaw=dist_yaw(engine);//*******************
         p_init.push_back(p);//***********
+        // printf("yaw:%lf\n",p.p_yaw);
+        // ROS_WARN_STREAM("yaw:" << p.p_yaw);
     }
     p_array=p_init;
 }
@@ -82,92 +82,95 @@ AMCL::Particle::Particle(int N) {
     weight = 1.0/double(N);
 }
 
-// void AMCL::p_init(AMCL::Particle &p, double x, double y, double yaw, double x_sigma, double y_sigma, double yaw_sigma)
-// {
-//     std::normal_distribution<> dist(x, x_sigma);
-//     p.pose.pose.pose.position.x=dist(engine)
-//     std::normal_distribution<> dist(y, y_sigma);
-//     p.pose.pose.pose.position.y=dist(engine)
-//     std::normal_distribution<> dist(yaw, yaw_sigma);
-//     tf::getYaw(p.pose.pose.pose.orientation)=dist(engine)//*******************
-// }
-
 void AMCL::p_motion_update()
 {
-    nav_msgs::Odometry current_odom;
-    nav_msgs::Odometry previous_odom;
     current_odom = odom;
     if(flag_first) previous_odom = current_odom;
     double dx=current_odom.pose.pose.position.x-previous_odom.pose.pose.position.x;
     double dy=current_odom.pose.pose.position.y-previous_odom.pose.pose.position.y;
     double current_yaw=tf::getYaw(current_odom.pose.pose.orientation);
-            // printf("1111\n");
     double previous_yaw=tf::getYaw(previous_odom.pose.pose.orientation);
-            // printf("111\n");
     double dyaw=current_yaw-previous_yaw;
+    // double dyaw=tf::getYaw(current_odom.pose.pose.orientation) - tf::getYaw(previous_odom.pose.pose.orientation);
     double weight_max = 0;
+    sum_weight = 0;
     p_move_array.clear();
+            // printf("current_yaw:%lf\n",current_yaw);
     for(auto& p_ : p_array) {
-        p_.p_pose.pose.pose.position.x += dx;
-        p_.p_pose.pose.pose.position.y += dy;
-        p_.p_yaw += dyaw;
+        std::normal_distribution<> dist_dx(dx, dxy_sigma);
+        std::normal_distribution<> dist_dy(dy, dxy_sigma);
+        std::normal_distribution<> dist_dyaw(dyaw, dyaw_sigma);
+
+        p_.p_pose.pose.pose.position.x += dist_dx(engine);
+        p_.p_pose.pose.pose.position.y += dist_dy(engine);
+        p_.p_yaw += dist_dyaw(engine);
+        geometry_msgs::Quaternion quat=tf::createQuaternionMsgFromYaw(p_.p_yaw);
+        p_.p_pose.pose.pose.orientation = quat;
         p_.weight = calc_weight(p_);
+        sum_weight += p_.weight;
+        // ROS_WARN_STREAM("yaw:" << p_.p_yaw);
         p_move_array.push_back(p_);
-            printf("b4\n");
 
         if(weight_max<p_.weight) {
-            printf("b3\n");
             weight_max = p_.weight;
+        // ROS_WARN_STREAM("weight:" << p_.weight);
             amcl_pose.pose.pose.position.x = p_.p_pose.pose.pose.position.x;
             amcl_pose.pose.pose.position.y = p_.p_pose.pose.pose.position.y;
             // tf::Quaternion quat= tf::createQuaternionFromRPY(0,0,p_.p_yaw);
-            geometry_msgs::Quaternion quat=tf::createQuaternionMsgFromYaw(p_.p_yaw);
-            amcl_pose.pose.pose.orientation = quat;
-            // amcl_pose.pose.pose.orientation = p_.p_pose.pose.pose.orientation;
+            // geometry_msgs::Quaternion quat=tf::createQuaternionMsgFromYaw(p_.p_yaw);
+            // amcl_pose.pose.pose.orientation = quat;
+            amcl_pose.pose.pose.orientation = p_.p_pose.pose.pose.orientation;
         }
     }
     flag_first = false;
     previous_odom = current_odom;
-            // printf("b4\n");
 }
 
 double AMCL::calc_weight(AMCL::Particle &pose)
 {
     double weight = 0;
-    // double yaw = tf::getYaw(pose.pose.orientation);
     for(int i=0; i<scan.ranges.size(); i+=scan.ranges.size()/ANGLE_STEP) {
-        // double x = p_pose.pose.pose.position.x+scan.ranges[i]*std::sin(scan.angle_min+i*scan.angle_increment);
-        double x = pose.p_pose.pose.pose.position.x+scan.ranges[i]*std::cos(scan.angle_min+i*scan.angle_increment+pose.p_yaw);
-        double y = pose.p_pose.pose.pose.position.y+scan.ranges[i]*std::sin(scan.angle_min+i*scan.angle_increment+pose.p_yaw);//sinとcos
-        double dist_ = std::sqrt(pow(x,2)+pow(y,2));
-        weight += gaussian(scan.ranges[i],range_sigma,dist_);
-        // if(scan.ranges[i]>scan.range_min) {
-            // double dist_to_wall = dist_p_to_obs();
+        if(1 == std::isinf(scan.ranges[i])) continue;
+        double angle = scan.angle_min + i * scan.angle_increment + pose.p_yaw;
+        double range = range_from_map(angle, pose);
+
+        // double x = pose.p_pose.pose.pose.position.x - map.info.origin.position.x + scan.ranges[i]*std::cos(scan.angle_min+i*scan.angle_increment+pose.p_yaw);
+        // double y = pose.p_pose.pose.pose.position.y + map.info.origin.position.y + scan.ranges[i]*std::sin(scan.angle_min+i*scan.angle_increment+pose.p_yaw);//sinとcos
+        // double dist_ = std::sqrt(pow(x,2)+pow(y,2));
+        weight += gaussian(scan.ranges[i],range_sigma,range);
     }
-            // printf("weight:%lf\n",weight/ANGLE_STEP);
     return weight/ANGLE_STEP;
+}
+
+double AMCL::range_from_map(double angle, AMCL::Particle pose)
+{
+    double p_x = pose.p_pose.pose.pose.position.x - map.info.origin.position.x;
+    double p_y = pose.p_pose.pose.pose.position.y - map.info.origin.position.y;
+    for(double range=0; range<=max_range;range += range_reso) {
+        double x = p_x + range*std::cos(angle);
+        double y = p_y + range*std::sin(angle);
+        int map_x = (int)(x / map.info.resolution);
+        int map_y = (int)(y / map.info.resolution);
+        // if(map_x>map.info.width || map_x <0) return range;
+        // if(map_y>map.info.height || map_y <0) return range;
+        if(map.data[map_x + map_y * map.info.width] != 0) return range;
+    }
+    return max_range;
 }
 
 double AMCL::gaussian(double mu, double sigma, double x)
 {
-    double ans = 1/std::sqrt(2*M_PI*pow(sigma,2))*exp(-pow(x-mu,2)/2*pow(sigma,2));
-    // printf("ans,%lf\n",ans);
+    double ans = 1e-10+1/std::sqrt(2*M_PI*pow(sigma,2))*exp(-pow(x-mu,2)/2/pow(sigma,2));
+    // printf("ans,%lf,%lf,%lf\n",mu,sigma,x);
     return ans;
 }
 
-// double AMCL::dist_p_to_obs(double x, double y, double yaw, double range)
-// {
-//     double search_step = map.info.resolution;
-//     // double search_max = std::min(range*, MAX_RANGE);
-//     for(double dist=search_step; dist<=MAX_RANGE; dist+=search_step) {
-//         x = x + dist * std::cos(yaw);
-//         y = y + dist * std::sin(yaw);
-//         int mass = map_to_mass(x, y);
-//
 void AMCL::resampring_selection()
 {
     resampring();
-    // reset();
+    // if(sum_weight/N>0.5) reset();
+    // double res = resampring();
+    // if(res<100) reset();
 }
 
 void AMCL::reset()
@@ -183,34 +186,23 @@ void AMCL::reset()
 
 void AMCL::resampring()
 {
-    std::uniform_real_distribution<> dist(0.0, 1.0);
+    std::uniform_real_distribution<> dist(0.0, sum_weight/N);
+        ROS_WARN_STREAM("weight:" << sum_weight/N);
     double r = dist(engine);
-    double sum_weight = 0;
+    double sum_w=0;
     std::vector<Particle> p_new_array;
     for(int i=0; i<N; i++) {
-        sum_weight += p_move_array[i].weight;
-        while(sum_weight>r) {
-            // printf("c3\n");
+        sum_w += p_move_array[i].weight;
+        while(sum_w>r && N != p_new_array.size()) {
             Particle p = p_move_array[i];
             p.weight = 1/double(N);
             p_new_array.push_back(p);
-            r += 1/double(N);
+            r += sum_weight/double(N);
         }
     }
     p_array = p_new_array;
-            printf("c1\n");
 }
 
-// void AMCL::output_pose()
-// {
-//     double weight_max = 0;
-//     for(auto& p : p_move_array) {
-//         if(weight_max<p.weight) {
-//             amcl_pose.pose.pose.position.x = p.x;
-//             amcl_pose.pose.pose.position.y = p.y;
-//             pub_amcl_pose.publish(amcl_pose);
-//
-// }
 void AMCL::output_pose()
 {
     for(auto& p : p_array) {
@@ -218,6 +210,7 @@ void AMCL::output_pose()
     }
     pub_amcl_pose.publish(amcl_pose);
     pub_pose_array.publish(pose_array);
+    pose_array.poses.clear();
 }
 
 void AMCL::process()
@@ -225,20 +218,14 @@ void AMCL::process()
     ros::Rate loop_rate(hz);
     while(ros::ok())
     {
-            printf("bool:%d\n",flag_first);
         if(flag_first) create_particles(init_x,init_y,init_yaw,init_x_sigma,init_y_sigma,init_yaw_sigma);
         if(flag_scan&&flag_odom&&flag_map) {
-            // printf("a1\n");
-            // if(std::isnan(p_array) printf("aaaaa\n");
             p_motion_update();
-            printf("a3\n");
             resampring_selection();
-            // printf("a4\n");
         }
         output_pose();
         ros::spinOnce();
         loop_rate.sleep();
-            printf("d1\n");
     }
 }
 
